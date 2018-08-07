d786c6a7e18c7455eef9b96d643e4bc0d42dee80
li-xirong <xirong.li@gmail.com>
2018-08-07 14:09:28 +0800

    add default values

--- basic/constant.py
+++ basic/constant.py
@@ -1,10 +1,10 @@
 import os
 
 # it raises an error if not found
-CODE_PATH = os.environ['SURVEY_CODE']
-ROOT_PATH = os.environ['SURVEY_DATA']
-OUTPUT_PATH = os.environ['SURVEY_DB']
-MATLAB_PATH = os.environ['MATLAB_PATH']
+CODE_PATH = os.environ['SURVEY_CODE'] if 'SURVEY_CODE' in os.environ else os.path.join(os.environ['HOME'], 'github', 'jingwei')
+ROOT_PATH = os.environ['SURVEY_DATA'] if 'SURVEY_DATA' in os.environ else os.path.join(os.environ['HOME'], 'VisualSearch')
+OUTPUT_PATH = os.environ['SURVEY_DB'] if 'SURVEY_DB' in os.environ else os.path.join(os.environ['HOME'], 'VisualSearch')
+MATLAB_PATH = os.environ['MATLAB_PATH'] if 'MATLAB_PATH' in os.environ else '/usr/local/'
 
 DEFAULT_TPP = 'lemm'
 DEFAULT_NEG_FILTER = 'co'
